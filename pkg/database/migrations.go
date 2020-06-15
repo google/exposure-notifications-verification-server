701d605d9cc5fb3e1cda07c1b864528d3225fa84
Mike Helmick <helmick@google.com>
2020-06-15 13:18:23 -0700

    fix spelling

--- pkg/database/migrations.go
+++ pkg/database/migrations.go
@@ -30,7 +30,7 @@ func (db *Database) RunMigrations(ctx context.Context) error {
 		{
 			ID: "00001-CreateUsers",
 			Migrate: func(tx *gorm.DB) error {
-				logger.Infof("db migrations: createing users table")
+				logger.Infof("db migrations: creating users table")
 				return tx.AutoMigrate(&User{}).Error
 			},
 			Rollback: func(tx *gorm.DB) error {
@@ -40,7 +40,7 @@ func (db *Database) RunMigrations(ctx context.Context) error {
 		{
 			ID: "00002-CreateVerificationCodes",
 			Migrate: func(tx *gorm.DB) error {
-				logger.Infof("db migrations: createing verificaion codes table")
+				logger.Infof("db migrations: creating verificaion codes table")
 				return tx.AutoMigrate(&VerificationCode{}).Error
 			},
 			Rollback: func(tx *gorm.DB) error {
@@ -50,7 +50,7 @@ func (db *Database) RunMigrations(ctx context.Context) error {
 		{
 			ID: "00003-CreateAuthorizedApps",
 			Migrate: func(tx *gorm.DB) error {
-				logger.Infof("db migrations: createing authorized apps table")
+				logger.Infof("db migrations: creating authorized apps table")
 				return tx.AutoMigrate(&AuthorizedApp{}).Error
 			},
 			Rollback: func(tx *gorm.DB) error {
