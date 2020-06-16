c6dbfb9c7f7e1d75b67602142f16205240963260
Mike Helmick <helmick@google.com>
2020-06-15 22:58:46 -0700

    vet

--- pkg/controller/apikey/apikey.go
+++ pkg/controller/apikey/apikey.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package apikey contains web controllers for listing and adding API Keys.
 package apikey
 
 import (
