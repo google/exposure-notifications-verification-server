e79e1d4aef5715e60bd9487b57e09ffa29e51c6c
Chris Wilcox <crwilcox@google.com>
2020-06-22 12:40:17 -0700

    add back error to apikey

--- pkg/controller/apikey/savekey.go
+++ pkg/controller/apikey/savekey.go
@@ -54,6 +54,7 @@ func (sc *apikeySaveController) ServeHTTP(w http.ResponseWriter, r *http.Request
 	}
 
 	if _, err := sc.db.CreateAuthorizedApp(form.Name); err != nil {
+		sc.logger.Errorf("error creating authorized app: %v", err)
 		flash.Error("Failed to create API key: %v", err)
 		return
 	}
