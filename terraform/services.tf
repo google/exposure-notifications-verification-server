34622ab470e0b23743192605acf96ac685d662ce
Mike Helmick <helmick@google.com>
2020-08-01 09:50:40 -0700

    Fix ordering

--- terraform/services.tf
+++ terraform/services.tf
@@ -22,7 +22,7 @@ locals {
   }
 
   session_config = {
-    COOKIE_KEYS = "secret://${google_secret_manager_secret_version.cookie-encryption-key-version.id},secret://${google_secret_manager_secret_version.cookie-hmac-key-version.id}"
+    COOKIE_KEYS = "secret://${google_secret_manager_secret_version.cookie-hmac-key-version.id},secret://${google_secret_manager_secret_version.cookie-encryption-key-version.id}"
   }
 
   database_config = {
