b8eee49507a014b4c0789b04960147f54bf8c5ed
Seth Vargo <seth@sethvargo.com>
2020-08-06 13:28:03 -0400

    Add KMS<->DB abstraction (#190)
    * Add generic password field
    
    * Always preload, add helper for detecting NotFound
    
    * Add key manager to database
    
    * Remove unnecessary preload tags
    
    * Move SMSConfig to KMS
    
    * SMSConfig owns realm (per gorm advice)
    
    * Setup default key manager for database
    
    * Use new model functions
    
    * Handle sms config in realmadmin
    
    * Remove add-sms-config helper (in UI now)
    
    * Update gorm
    
    * Add Terraform
    
    * Add a migration path
    
    * Update message

--- terraform/service_apiserver.tf
+++ terraform/service_apiserver.tf
@@ -61,6 +61,12 @@ resource "google_project_iam_member" "apiserver-observability" {
   member  = "serviceAccount:${google_service_account.apiserver.email}"
 }
 
+resource "google_kms_crypto_key_iam_member" "apiserver-database-encrypter" {
+  crypto_key_id = google_kms_crypto_key.database-encrypter.self_link
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  member        = "serviceAccount:${google_service_account.apiserver.email}"
+}
+
 resource "google_cloud_run_service" "apiserver" {
   name     = "apiserver"
   location = var.region
