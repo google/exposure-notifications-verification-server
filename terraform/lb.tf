6500a7203e60aa2b5d602e87ca42549389cc9d42
Nat Welch <natwelch@google.com>
2020-08-16 19:03:19 -0400

    Various fixes

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -1,33 +1,44 @@
 # https://github.com/hashicorp/terraform-provider-google-beta/pull/2348
 # https://cloud.google.com/load-balancing/docs/negs/setting-up-serverless-negs
 resource "google_compute_region_network_endpoint_group" "apiserver-us-central1" {
-  provider = google-beta
-}
-
-resource "google_compute_http_health_check" "healthz" {
-  name               = "healthz"
-  request_path       = "/healthz"
-  check_interval_sec = 10
-  timeout_sec        = 1
+  provider  = google-beta
+  region    = "us-central1"
+  cloud_run = "apiserver"
 }
 
 resource "google_compute_backend_service" "apiserver" {
-  name          = "apiserver"
-  health_checks = [google_compute_http_health_check.healthz.id]
+  name       = "apiserver"
+  enable_cdn = true
 
   backend {
     group = google_compute_region_network_endpoint_group.apiserver-us-central1.id
   }
 }
 
-resource "google_compute_global_forwarding_rule" "apiserver-http" {
-  name       = "apiserver-http"
-  target     = google_compute_backend_service.apiserver.self_link
-  ip_address = google_compute_global_address.apiserver.address
-  port_range = "80"
-  depends_on = [google_compute_global_address.apiserver]
+resource "google_compute_global_address" "verification-server" {
+  name = "verification-server-address"
 }
 
-resource "google_compute_global_address" "apiserver" {
-  name = "apiserver-address"
+resource "google_compute_url_map" "urlmap" {
+  name = "verification-server"
+
+  default_service = google_compute_backend_service.apiserver.id
+
+  // TODO(icco): Add host base routing for all four services.
+}
+
+resource "google_compute_target_http_proxy" "default" {
+  url_map = google_compute_url_map.urlmap.id
+}
+
+resource "google_compute_forwarding_rule" "verification-server" {
+  provider = google-beta
+  name     = "verification-server"
+
+  ip_protocol           = "TCP"
+  ip_address            = google_compute_global_address.verification-server.address
+  load_balancing_scheme = "EXTERNAL"
+  port_range            = "80"
+  target                = google_compute_region_target_http_proxy.default.id
+  network_tier          = "PREMIUM"
 }
