f8b60029223127bbc6dc8cad0d9525a4ae7b8676
Nat Welch <natwelch@google.com>
2020-08-18 11:18:41 -0400

    Add project

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -3,14 +3,17 @@
 resource "google_compute_region_network_endpoint_group" "apiserver" {
   name     = "api-server"
   provider = google-beta
+  project  = var.project
   region   = var.region
+
   cloud_run {
-    service = "apiserver"
+    service = google_cloud_run_service.apiserver.name
   }
 }
 
 resource "google_compute_backend_service" "apiserver" {
   name       = "apiserver"
+  project    = var.project
   enable_cdn = true
 
   backend {
@@ -19,11 +22,13 @@ resource "google_compute_backend_service" "apiserver" {
 }
 
 resource "google_compute_global_address" "verification-server" {
-  name = "verification-server-address"
+  name    = "verification-server-address"
+  project = var.project
 }
 
 resource "google_compute_url_map" "urlmap" {
   name            = "verification-server"
+  project         = var.project
   default_service = google_compute_backend_service.apiserver.id
 
   // TODO(icco): Add host base routing for all four services.
@@ -31,12 +36,14 @@ resource "google_compute_url_map" "urlmap" {
 
 resource "google_compute_target_http_proxy" "default" {
   name    = "verification-server"
+  project = var.project
   url_map = google_compute_url_map.urlmap.id
 }
 
 resource "google_compute_forwarding_rule" "verification-server" {
   provider = google-beta
   name     = "verification-server"
+  project  = var.project
 
   ip_protocol           = "TCP"
   ip_address            = google_compute_global_address.verification-server.address
