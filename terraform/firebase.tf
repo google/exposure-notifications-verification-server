df753093813ce3c0de61b8c2f0555d65e02284e4
Seth Vargo <seth@sethvargo.com>
2020-08-21 18:04:32 -0400

    Output all firebase values

--- terraform/firebase.tf
+++ terraform/firebase.tf
@@ -31,3 +31,35 @@ data "google_firebase_web_app_config" "default" {
   provider   = google-beta
   web_app_id = google_firebase_web_app.default.app_id
 }
+
+output "firebase_app_id" {
+  value = google_firebase_web_app.default.app_id
+}
+
+output "firebase_project_id" {
+  value = google_firebase_web_app.default.project
+}
+
+output "firebase_api_key" {
+  value = data.google_firebase_web_app_config.default.api_key
+}
+
+output "firebase_auth_domain" {
+  value = data.google_firebase_web_app_config.default.auth_domain
+}
+
+output "firebase_database_url" {
+  value = data.google_firebase_web_app_config.default.database_url
+}
+
+output "firebase_measurement_id" {
+  value = data.google_firebase_web_app_config.default.measurement_id
+}
+
+output "firebase_messaging_sender_id" {
+  value = data.google_firebase_web_app_config.default.messaging_sender_id
+}
+
+output "firebase_storage_bucket" {
+  value = data.google_firebase_web_app_config.default.storage_bucket
+}
