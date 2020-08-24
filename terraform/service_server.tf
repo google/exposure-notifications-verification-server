89020aa15be2e0ac945f4f23127ced60ded47d3f
Nat Welch <natwelch@google.com>
2020-08-24 13:14:00 -0400

    Set request timeout for services behind a GCLB to 25s (#336)

--- terraform/service_server.tf
+++ terraform/service_server.tf
@@ -113,6 +113,7 @@ resource "google_cloud_run_service" "server" {
   template {
     spec {
       service_account_name = google_service_account.server.email
+      timeout_seconds      = 25
 
       containers {
         image = "gcr.io/${var.project}/github.com/google/exposure-notifications-verification-server/cmd/server:initial"
