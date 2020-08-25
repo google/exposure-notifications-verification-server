cdf055c3b5d2a9d4b6f91469e64885a9b8963563
Mike Helmick <helmick@google.com>
2020-08-25 09:15:00 -0700

    need to upgrade realm in migrations that cause realm to be addressed (#357)

--- pkg/database/migrations.go
+++ pkg/database/migrations.go
@@ -716,6 +716,9 @@ func (db *Database) getMigrations(ctx context.Context) *gormigrate.Gormigrate {
 			ID: "00030-HMACCodes",
 			Migrate: func(tx *gorm.DB) error {
 				logger.Debugw("db migrations: HMACing existing tokens")
+				if err := tx.AutoMigrate(&Realm{}).Error; err != nil {
+					return err
+				}
 
 				var codes []VerificationCode
 				if err := tx.Model(VerificationCode{}).Find(&codes).Error; err != nil {
