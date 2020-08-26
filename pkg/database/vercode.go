b2c79b5cad33b4ace2e86d77eb8f9594c35668ed
Weston Haught <whaught@google.com>
2020-08-26 10:02:23 -0700

    Add code expire endpoint

--- pkg/database/vercode.go
+++ pkg/database/vercode.go
@@ -195,6 +195,29 @@ func (db *Database) FindVerificationCodeByUUID(uuid string) (*VerificationCode, 
 	return &vc, nil
 }
 
+// ExpireCode saves a verification code as expired.
+func (db *Database) ExpireCode(uuid string) (*VerificationCode, error) {
+	var vc VerificationCode
+	err := db.db.Transaction(func(tx *gorm.DB) error {
+		if err := tx.
+			Set("gorm:query_option", "FOR UPDATE").
+			Where("uuid = ?", uuid).
+			Find(&vc).Error; err != nil {
+			return err
+		}
+
+		vc.ExpiresAt = time.Now()
+		vc.LongExpiresAt = vc.ExpiresAt
+
+		return tx.Save(&vc).Error
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &vc, nil
+}
+
 // SaveVerificationCode created or updates a verification code in the database.
 // Max age represents the maximum age of the test date [optional] in the record.
 func (db *Database) SaveVerificationCode(vc *VerificationCode, maxAge time.Duration) error {
