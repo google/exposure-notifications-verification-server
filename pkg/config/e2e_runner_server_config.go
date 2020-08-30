583963b0bca3afe6a2d3a166dcd959758a4248b1
Yuchen Ying <github.com@yegle.net>
2020-08-30 16:45:41 -0700

    Create e2e-runner service. (#417)
    * Create e2e-runner service.
    
    This commit refactored the existing e2e-test CLI into a web service.
    
    It currently provides /default and /revise handlers, maps to the
    `-revise=false` and `-revise=true` flag of the original CLI.
    
    This service is currently guarded by an IAM policy to avoid hammering
    the API servers repeatedly.
    
    * small fix: remove unused code.
    
    * Replace Infof with Debugw per review suggestion.
    
    * Address additional comment from review.
    
    * Add observability configs.

---
+++ pkg/config/e2e_runner_server_config.go
@@ -0,0 +1,60 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"context"
+
+	"github.com/google/exposure-notifications-server/pkg/observability"
+	"github.com/google/exposure-notifications-verification-server/pkg/database"
+	"github.com/sethvargo/go-envconfig"
+)
+
+// E2ERunnerConfig represents the environment based configuration for the e2e-runner server.
+type E2ERunnerConfig struct {
+	Database      database.Config
+	Observability *observability.Config
+
+	// DevMode produces additional debugging information. Do not enable in
+	// production environments.
+	DevMode bool `env:"DEV_MODE"`
+
+	Port string `env:"PORT,default=8080"`
+
+	// e2e-runner config
+	VerificationAdminAPIServer string `env:"VERIFICATION_ADMIN_API, default=http://localhost:8081"`
+	VerificationAdminAPIKey    string
+	VerificationAPIServer      string `env:"VERIFICATION_SERVER_API, default=http://localhost:8082"`
+	VerificationAPIServerKey   string
+	KeyServer                  string `env:"KEY_SERVER, default=http://localhost:8080"`
+	HealthAuthorityCode        string `env:"HEALTH_AUTHORITY_CODE,required"`
+
+	// e2e-test config
+	DoRevise bool
+}
+
+// NewE2ERunnerConfig returns the environment config for the e2e-runner server.
+// Only needs to be called once per instance, but may be called multiple times.
+func NewE2ERunnerConfig(ctx context.Context) (*E2ERunnerConfig, error) {
+	var config E2ERunnerConfig
+	if err := ProcessWith(ctx, &config, envconfig.OsLookuper()); err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
+
+func (c *E2ERunnerConfig) Validate() error {
+	return nil
+}
