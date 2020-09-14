eaf4539ed038c55a376d4a48f7ed3283fc1bc2e7
Yuchen Ying <github.com@yegle.net>
2020-09-14 14:04:18 -0700

    Add variable monitoring-host-project to alerting module.
    For multi-project workspaces, a separate empty monitoring host project
    is preferred.

--- terraform/alerting/notifications.tf
+++ terraform/alerting/notifications.tf
@@ -1,6 +1,6 @@
 resource "google_monitoring_notification_channel" "email" {
   provider     = google-beta
-  project      = var.project
+  project      = var.monitoring-host-project
   display_name = "Email Notification Channel"
   type         = "email"
   labels = {
