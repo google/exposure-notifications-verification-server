123c1dc3654ceaf27abd7f022ce88cc084f16e6b
Seth Vargo <seth@sethvargo.com>
2020-09-25 13:51:02 -0400

    Simpler logic

--- pkg/controller/realmadmin/settings.go
+++ pkg/controller/realmadmin/settings.go
@@ -121,19 +121,15 @@ func (c *Controller) HandleSettings() http.Handler {
 		if form.Codes {
 			realm.AllowedTestTypes = form.AllowedTestTypes
 			realm.RequireDate = form.RequireDate
-			if v := realm.CodeLength; v > 0 {
-				realm.CodeLength = v
-			}
-			if v := form.CodeDurationMinutes; v > 0 {
-				realm.CodeDuration.Duration = time.Duration(v) * time.Minute
-			}
-			if v := form.LongCodeLength; v > 0 {
-				realm.LongCodeLength = v
-			}
-			if v := form.LongCodeDurationHours; v > 0 {
-				realm.LongCodeDuration.Duration = time.Duration(v) * time.Hour
-			}
 			realm.SMSTextTemplate = form.SMSTextTemplate
+
+			// These fields can only be set if ENX is disabled
+			if !realm.EnableENExpress {
+				realm.CodeLength = form.CodeLength
+				realm.CodeDuration.Duration = time.Duration(form.CodeDurationMinutes) * time.Minute
+				realm.LongCodeLength = form.LongCodeLength
+				realm.LongCodeDuration.Duration = time.Duration(form.LongCodeDurationHours) * time.Hour
+			}
 		}
 
 		// SMS
