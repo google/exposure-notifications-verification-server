abd91235b291935a6dda8c326632822cdebc356b
Weston Haught <whaught@google.com>
2020-10-06 11:45:19 -0700

    Recreate firebase user, no metric (#756)
    * Recreate firebase user, no metric
    
    * use the shared method
    
    * err message

---
+++ pkg/controller/user/reset_password.go
@@ -0,0 +1,58 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package user
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/exposure-notifications-verification-server/pkg/controller"
+	"github.com/google/exposure-notifications-verification-server/pkg/database"
+)
+
+func (c *Controller) HandleResetPassword() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.Show(w, r, true)
+	})
+}
+
+func (c *Controller) resetPassword(ctx context.Context, user *database.User) (bool, error) {
+	return c.maybeResetPassword(ctx, true, user)
+}
+
+func (c *Controller) createFirebaseUser(ctx context.Context, user *database.User) (bool, error) {
+	return c.maybeResetPassword(ctx, false, user)
+}
+
+func (c *Controller) maybeResetPassword(ctx context.Context, reset bool, user *database.User) (bool, error) {
+	session := controller.SessionFromContext(ctx)
+	flash := controller.Flash(session)
+
+	// Ensure the firebase user is created
+	created, err := user.CreateFirebaseUser(ctx, c.client)
+	if err != nil {
+		flash.Alert("Failed to create user auth: %v", err)
+		return created, err
+	}
+
+	// Reset if we just created or a reset was asked for.
+	if created || reset {
+		if err := c.firebaseInternal.SendPasswordResetEmail(ctx, user.Email); err != nil {
+			flash.Error("Could not send new user invitation: %v", err)
+			return true, err
+		}
+	}
+	return created, nil
+}
