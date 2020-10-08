e197bfef711e2675d0955faf4b84f831c763bb7d
Yuchen Ying <github.com@yegle.net>
2020-10-08 12:38:34 -0700

    --wip-- [skip ci]

--- pkg/controller/certapi/metrics.go
+++ pkg/controller/certapi/metrics.go
@@ -26,60 +26,11 @@ import (
 const metricPrefix = observability.MetricRoot + "/api/certificate"
 
 var (
-	mAttempts          = stats.Int64(metricPrefix+"/attempts", "certificate issue attempts", stats.UnitDimensionless)
-	mTokenExpired      = stats.Int64(metricPrefix+"/token_expired", "expired tokens on certificate issue", stats.UnitDimensionless)
-	mTokenUsed         = stats.Int64(metricPrefix+"/token_used", "already used tokens on certificate issue", stats.UnitDimensionless)
-	mTokenInvalid      = stats.Int64(metricPrefix+"/invalid_token", "invalid tokens on certificate issue", stats.UnitDimensionless)
-	mCertificateIssued = stats.Int64(metricPrefix+"/issue", "certificates issued", stats.UnitDimensionless)
-	mCertificateErrors = stats.Int64(metricPrefix+"/errors", "certificate issue errors", stats.UnitDimensionless)
-
 	mRequest = stats.Int64(metricPrefix+"/request", "# of certificate issue requests", stats.UnitDimensionless)
 )
 
 func init() {
 	enobservability.CollectViews([]*view.View{
-		{
-			Name:        metricPrefix + "/attempt_count",
-			Measure:     mAttempts,
-			Description: "The count of certificate issue attempts",
-			TagKeys:     observability.CommonTagKeys(),
-			Aggregation: view.Count(),
-		},
-		{
-			Name:        metricPrefix + "/token_expired_count",
-			Measure:     mTokenExpired,
-			Description: "The count of expired tokens on certificate issue",
-			TagKeys:     observability.CommonTagKeys(),
-			Aggregation: view.Count(),
-		},
-		{
-			Name:        metricPrefix + "/token_used_count",
-			Measure:     mTokenUsed,
-			Description: "The count of already used tokens on certificate issue",
-			TagKeys:     observability.CommonTagKeys(),
-			Aggregation: view.Count(),
-		},
-		{
-			Name:        metricPrefix + "/invalid_token_count",
-			Measure:     mTokenInvalid,
-			Description: "The count of invalid tokens on certificate issue",
-			TagKeys:     observability.CommonTagKeys(),
-			Aggregation: view.Count(),
-		},
-		{
-			Name:        metricPrefix + "/issue_count",
-			Measure:     mCertificateIssued,
-			Description: "The count of certificates issued",
-			TagKeys:     observability.CommonTagKeys(),
-			Aggregation: view.Count(),
-		},
-		{
-			Name:        metricPrefix + "/error_count",
-			Measure:     mCertificateErrors,
-			Description: "The count of certificate issue errors",
-			TagKeys:     observability.CommonTagKeys(),
-			Aggregation: view.Count(),
-		},
 		{
 			Name:        metricPrefix + "/request_count",
 			Measure:     mRequest,
