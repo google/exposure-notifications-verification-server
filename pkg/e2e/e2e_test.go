863884e505ea27ed77565e0ef2ee520ee9ababfe
Charles Chen <pyc@google.com>
2020-10-12 22:41:06 -0700

    Add E2E tests for verification server.

---
+++ pkg/e2e/e2e_test.go
@@ -0,0 +1,99 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package e2e
+
+import (
+	"context"
+	"encoding/base64"
+	"testing"
+	"time"
+
+	verifyapi "github.com/google/exposure-notifications-server/pkg/api/v1"
+	"github.com/google/exposure-notifications-server/pkg/util"
+	"github.com/google/exposure-notifications-server/pkg/verification"
+	"github.com/google/exposure-notifications-verification-server/pkg/api"
+)
+
+const (
+	oneDay         = 24 * time.Hour
+	intervalLength = 10 * time.Minute
+	maxInterval    = 144
+)
+
+func TestEndpoint(t *testing.T) {
+	ctx := context.Background()
+
+	suite := NewE2ESuite(t, ctx)
+	adminClient := suite.NewAdminAPIClient()
+	apiClient := suite.NewAPIClient()
+
+	now := time.Now().UTC()
+	curDayInterval := timeToInterval(now)
+	nextInterval := curDayInterval
+	symptomDate := time.Now().UTC().Add(-48 * time.Hour).Format("2006-01-02")
+	testType := "confirmed"
+	tzMinOffset := 0
+
+	teks := make([]verifyapi.ExposureKey, 14)
+	for i := 0; i < len(teks); i++ {
+		key, err := util.RandomExposureKey(nextInterval, maxInterval, 0)
+		if err != nil {
+			t.Fatalf("not enough entropy: %v", err)
+		}
+		teks[i] = key
+		nextInterval -= maxInterval
+	}
+
+	hmacSecret := make([]byte, 32)
+	hmacValue, err := verification.CalculateExposureKeyHMAC(teks, hmacSecret)
+	if err != nil {
+		t.Fatalf("error calculating tek HMAC: %v", err)
+	}
+	hmacB64 := base64.StdEncoding.EncodeToString(hmacValue)
+
+	issueRequest := api.IssueCodeRequest{
+		TestType:    testType,
+		SymptomDate: symptomDate,
+		TZOffset:    float32(tzMinOffset),
+	}
+
+	issueResp, err := adminClient.IssueCode(issueRequest)
+	if issueResp == nil || err != nil {
+		t.Fatalf("adminClient.IssueCode(%+v) = expected nil, got resp %+v, err %v", issueRequest, issueResp, err)
+	}
+
+	verifyRequest := api.VerifyCodeRequest{
+		VerificationCode: issueResp.VerificationCode,
+		AcceptTestTypes:  []string{testType},
+	}
+
+	verifyResp, err := apiClient.GetToken(verifyRequest)
+	if err != nil {
+		t.Fatalf("apiClient.GetToken(%+v) = expected nil, got resp %+v, err %v", verifyRequest, verifyResp, err)
+	}
+
+	certRequest := api.VerificationCertificateRequest{
+		VerificationToken: verifyResp.VerificationToken,
+		ExposureKeyHMAC:   hmacB64,
+	}
+	_, err = apiClient.GetCertificate(certRequest)
+	if err != nil {
+		t.Errorf("apiClient.GetCertificate(%+v) = expected nil, got err %v", certRequest, err)
+	}
+}
+
+func timeToInterval(t time.Time) int32 {
+	return int32(t.UTC().Truncate(oneDay).Unix() / int64(intervalLength.Seconds()))
+}
