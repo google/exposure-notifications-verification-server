68266510df019890b0fdaa85635c711b6ee9e908
Nat Welch <natwelch@google.com>
2020-10-16 17:41:47 -0400

    typo

--- terraform/verification-lb.tf
+++ terraform/verification-lb.tf
@@ -128,7 +128,7 @@ resource "google_compute_target_https_proxy" "https" {
 
   url_map          = google_compute_url_map.urlmap-https[0].id
   ssl_certificates = [google_compute_managed_ssl_certificate.default[0].id]
-  ssl_policy       = google_compute_ssl_policy.one-two-ssl-policy
+  ssl_policy       = google_compute_ssl_policy.one-two-ssl-policy.id
 }
 
 resource "google_compute_global_forwarding_rule" "http" {
