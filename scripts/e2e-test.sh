4709e88e7d9a559a3c5280754266459e1b4e28b0
Charles Chen <pyc@google.com>
2020-10-16 18:33:04 -0700

    typo

--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -47,7 +47,7 @@ function incremental() {
   export_terraform_output db_password E2E_DB_PASSWORD
   export_terraform_output db_apikey_database_key_secret E2E_DB_APIKEY_DATABASE_KEY
   export_terraform_output db_apikey_signature_key_secret E2E_DB_APIKEY_SIGNATURE_KEY
-  export_terraform_output db_encreption_key_secret E2E_DB_ENCRYPTION_KEY
+  export_terraform_output db_encryption_key_secret E2E_DB_ENCRYPTION_KEY
   export_terraform_output db_verification_code_key_secret E2E_DB_VERIFICATION_CODE_DATABASE_KEY
   export_terraform_output apiserver_urls[0] E2E_APISERVER_URL
   export_terraform_output adminapi_urls[0] E2E_ADMINAPI_URL
