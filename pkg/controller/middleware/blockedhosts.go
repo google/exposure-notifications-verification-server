f4669c4f9e016af801885d3452171b541cfcd442
Weston Haught <whaught@google.com>
2020-10-26 11:42:53 -0700

    url hostname

--- pkg/controller/middleware/blockedhosts.go
+++ pkg/controller/middleware/blockedhosts.go
@@ -17,7 +17,6 @@ package middleware
 
 import (
 	"context"
-	"net"
 	"net/http"
 	"strings"
 
@@ -39,13 +38,8 @@ func CheckBlockedHosts(ctx context.Context, blockedHosts []string, h *render.Ren
 				next.ServeHTTP(w, r)
 			}
 
-			rHost, _, err := net.SplitHostPort(r.Host)
-			if err != nil { // No port or invalid address
-				rHost = r.Host
-			}
-
 			for _, host := range blockedHosts {
-				if strings.HasSuffix(rHost, host) {
+				if strings.HasSuffix(r.URL.Hostname(), host) {
 					logger.Warnf("received request from blocked domain %s", r.Host)
 					controller.Unauthorized(w, r, h)
 					return
