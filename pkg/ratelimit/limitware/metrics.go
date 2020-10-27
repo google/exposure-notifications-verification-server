1aa9e0e059c5fc546b4a3157d10d669e43d4f5ad
Yuchen Ying <github.com@yegle.net>
2020-10-27 11:01:09 -0700

    Group imports.

--- pkg/ratelimit/limitware/metrics.go
+++ pkg/ratelimit/limitware/metrics.go
@@ -16,7 +16,9 @@ package limitware
 
 import (
 	enobservability "github.com/google/exposure-notifications-server/pkg/observability"
+
 	"github.com/google/exposure-notifications-verification-server/pkg/observability"
+
 	"github.com/opencensus-integrations/redigo/redis"
 	"go.opencensus.io/stats"
 	"go.opencensus.io/stats/view"
