58820fedc087f615c90573cdb43614e160b750af
Yuchen Ying <github.com@yegle.net>
2020-11-03 09:05:05 -0800

    Generalize the RecordLatency function to be reused later. (#943)
    * Generalize the RecordLatency function to be reused later.
    
    * Be explicit and eliminate the use of a closure.
    
    * Call the function instead of returning the function.
    
    * Add additional comments.

--- pkg/clients/e2e.go
+++ pkg/clients/e2e.go
@@ -25,7 +25,6 @@ import (
 	"github.com/google/exposure-notifications-verification-server/pkg/config"
 	"github.com/google/exposure-notifications-verification-server/pkg/jsonclient"
 	"github.com/google/exposure-notifications-verification-server/pkg/observability"
-	"go.opencensus.io/stats"
 	"go.opencensus.io/tag"
 
 	verifyapi "github.com/google/exposure-notifications-server/pkg/api/v1"
@@ -43,15 +42,6 @@ const (
 
 func timeToInterval(t time.Time) int32 {
 	return int32(t.UTC().Truncate(oneDay).Unix() / int64(intervalLength.Seconds()))
-}
-
-func recordLatency(step string) func(context.Context, *tag.Mutator) {
-	start := time.Now()
-	return func(ctx context.Context, result *tag.Mutator) {
-		latency := float64(time.Since(start)) / float64(time.Millisecond)
-		step := tag.Upsert(stepTagKey, step)
-		stats.RecordWithTags(ctx, []tag.Mutator{*result, step}, mLatencyMs.M(latency))
-	}
 }
 
 // RunEndToEnd - code that exercises the verification and key server, simulating a
@@ -82,6 +72,13 @@ func RunEndToEnd(ctx context.Context, config *config.E2ETestConfig) error {
 		nextInterval -= maxInterval
 	}
 
+	result := observability.ResultOK()
+	// Parameterize observability.RecordLatency()
+	recordLatency := func(ctx context.Context, start time.Time, step string) {
+		stepMutator := tag.Upsert(stepTagKey, step)
+		observability.RecordLatency(ctx, start, mLatencyMs, &stepMutator, &result)
+	}
+
 	for i := 0; i < iterations; i++ {
 		logger.Infof("Issuing verification code, iteration %d", i)
 
@@ -90,8 +87,7 @@ func RunEndToEnd(ctx context.Context, config *config.E2ETestConfig) error {
 			return fmt.Errorf("unable to create new context with additional tags: %w", err)
 		}
 		code, err := func() (*api.IssueCodeResponse, error) {
-			result := observability.ResultOK()
-			defer recordLatency("/api/issue")(ctx, &result)
+			defer recordLatency(ctx, time.Now(), "/api/issue")
 			// Issue the verification code.
 			codeRequest, code, err := IssueCode(ctx, config.VerificationAdminAPIServer, config.VerificationAdminAPIKey, testType, symptomDate, 0, timeout)
 			if err != nil {
@@ -113,8 +109,7 @@ func RunEndToEnd(ctx context.Context, config *config.E2ETestConfig) error {
 		}
 
 		token, err := func() (*api.VerifyCodeResponse, error) {
-			result := observability.ResultOK()
-			defer recordLatency("/api/verify")(ctx, &result)
+			defer recordLatency(ctx, time.Now(), "/api/verify")
 			// Get the verification token
 			logger.Infof("Verifying code and getting token")
 			tokenRequest, token, err := GetToken(ctx, config.VerificationAPIServer, config.VerificationAPIServerKey, code.VerificationCode, timeout)
@@ -136,8 +131,7 @@ func RunEndToEnd(ctx context.Context, config *config.E2ETestConfig) error {
 		}
 
 		if err := func() error {
-			result := observability.ResultOK()
-			defer recordLatency("/api/verify")(ctx, &result)
+			defer recordLatency(ctx, time.Now(), "/api/verify")
 			logger.Infof("Check code status")
 			statusReq, codeStatus, err := CheckCodeStatus(ctx, config.VerificationAdminAPIServer, config.VerificationAdminAPIKey, code.UUID, timeout)
 			if err != nil {
@@ -161,8 +155,7 @@ func RunEndToEnd(ctx context.Context, config *config.E2ETestConfig) error {
 		}
 
 		hmacSecret, hmacB64, err := func() ([]byte, string, error) {
-			result := observability.ResultOK()
-			defer recordLatency("hmac")(ctx, &result)
+			defer recordLatency(ctx, time.Now(), "hmac")
 			logger.Infof("Calculating HMAC")
 			hmacSecret := make([]byte, 32)
 			hmacValue, err := verification.CalculateExposureKeyHMAC(teks, hmacSecret)
@@ -177,8 +170,7 @@ func RunEndToEnd(ctx context.Context, config *config.E2ETestConfig) error {
 		}
 
 		certificate, err := func() (*api.VerificationCertificateResponse, error) {
-			result := observability.ResultOK()
-			defer recordLatency("/api/certificate")(ctx, &result)
+			defer recordLatency(ctx, time.Now(), "/api/certificate")
 			logger.Infof("Getting verification certificate")
 			// Get the verification certificate
 			certRequest, certificate, err := GetCertificate(ctx, config.VerificationAPIServer, config.VerificationAPIServerKey, token.VerificationToken, hmacB64, timeout)
@@ -209,8 +201,7 @@ func RunEndToEnd(ctx context.Context, config *config.E2ETestConfig) error {
 		}
 
 		response, err := func() (*verifyapi.PublishResponse, error) {
-			result := observability.ResultOK()
-			defer recordLatency("upload_to_key_server")(ctx, &result)
+			defer recordLatency(ctx, time.Now(), "upload_to_key_server")
 			// Make the publish request.
 			logger.Infof("Publish TEKs to the key server")
 			var response verifyapi.PublishResponse
--- pkg/observability/observability.go
+++ pkg/observability/observability.go
@@ -19,9 +19,11 @@ import (
 	"context"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/google/exposure-notifications-server/pkg/logging"
 	"github.com/google/exposure-notifications-verification-server/pkg/buildinfo"
+	"go.opencensus.io/stats"
 	"go.opencensus.io/tag"
 )
 
@@ -150,4 +152,21 @@ func WithBuildInfo(octx context.Context) context.Context {
 	}
 
 	return ctx
+}
+
+// RecordLatency calculate and record the latency.
+// Usage example:
+// func foo() {
+// 	 defer RecordLatency(ctx, time.Now(), metric, tag1, tag2)
+//   // remaining of the function body.
+// }
+func RecordLatency(ctx context.Context, start time.Time, m *stats.Float64Measure, mutators ...*tag.Mutator) {
+	var additionalMutators []tag.Mutator
+	for _, t := range mutators {
+		additionalMutators = append(additionalMutators, *t)
+	}
+	// Calculate the millisecond number as float64. time.Duration.Millisecond()
+	// returns an integer.
+	latency := float64(time.Since(start)) / float64(time.Millisecond)
+	stats.RecordWithTags(ctx, additionalMutators, m.M(latency))
 }
