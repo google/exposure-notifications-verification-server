490ac66272fcc4c2adfe3eaf7d17d7919791d85e
Seth Vargo <seth@sethvargo.com>
2020-11-06 16:17:19 -0500

    Authenticate to private Docker on CI

--- scripts/presubmit.sh
+++ scripts/presubmit.sh
@@ -28,10 +28,10 @@ export GOMAXPROCS=7
 if [ -n "${CI:-}" ]; then
   gcloud --quiet auth configure-docker us-docker.pkg.dev
 fi
-if [ -n "${CI_POSTGRES_IMAGE}:-" ]; then
+if [ -n "${CI_POSTGRES_IMAGE:-}" ]; then
   docker pull --quiet us-docker.pkg.dev/apollo-verification-us/mirrors/postgres:12-alpine
 fi
-if [ -n "${CI_REDIS_IMAGE}:-" ]; then
+if [ -n "${CI_REDIS_IMAGE:-}" ]; then
   docker pull --quiet us-docker.pkg.dev/apollo-verification-us/mirrors/redis:6-alpine
 fi
 
