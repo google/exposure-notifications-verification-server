af659464d08ab65ca3919078faf4b50cfe750713
Weston Haught <whaught@google.com>
2020-11-12 21:59:32 -0700

    Accept client UUID for issued-code tracking (#1086)
    * Accept client UUID for issued-code tracking
    
    * conflict if exists
    
    * use a unique index
    
    * whitespace
    
    * update API doc
    
    * update with error code

--- pkg/controller/issueapi/issue.go
+++ pkg/controller/issueapi/issue.go
@@ -192,7 +192,7 @@ func (c *Controller) HandleIssue() http.Handler {
 			return
 		}
 
-		// Set up parallel arrays to leverage the observability reporting and connect the parse / valdiation errors
+		// Set up parallel arrays to leverage the observability reporting and connect the parse / validation errors
 		// to the correct date.
 		parsedDates := make([]*time.Time, 2)
 		input := []string{request.SymptomDate, request.TestDate}
@@ -290,14 +290,22 @@ func (c *Controller) HandleIssue() http.Handler {
 			IssuingUser:    user,
 			IssuingApp:     authApp,
 			RealmID:        realm.ID,
+			UUID:           request.UUID,
 		}
 
 		code, longCode, uuid, err := codeRequest.Issue(ctx, c.config.GetCollisionRetryCount())
 		if err != nil {
 			logger.Errorw("failed to issue code", "error", err)
-			c.h.RenderJSON(w, http.StatusInternalServerError, api.Errorf("failed to generate otp code, please try again"))
 			blame = observability.BlameServer
 			result = observability.ResultError("FAILED_TO_ISSUE_CODE")
+
+			// GormV1 doesn't have a good way to match db errors
+			if strings.Contains(err.Error(), database.VercodeUUIDUniqueIndex) {
+				c.h.RenderJSON(w, http.StatusConflict,
+					api.Errorf("code for %s already exists", request.UUID).WithCode(api.ErrUUIDAlreadyExists))
+				return
+			}
+			c.h.RenderJSON(w, http.StatusInternalServerError, api.Errorf("failed to generate otp code, please try again"))
 			return
 		}
 
