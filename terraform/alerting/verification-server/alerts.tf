23d9543f92919ba46f40b201782b215e0b6893db
Yuri Grinshteyn <ygrinshteyn@google.com>
2020-11-13 09:54:11 -0800

    commenting out alerting to get apply to work

--- terraform/alerting/verification-server/alerts.tf
+++ terraform/alerting/verification-server/alerts.tf
@@ -12,48 +12,48 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-resource "google_monitoring_alert_policy" "RealmTokenRemainingCapacityLow" {
-  project      = var.verification-server-project
-  combiner     = "OR"
-  display_name = "RealmTokenRemainingCapacityLow"
-  conditions {
-    display_name = "Per-realm issue API token remaining capacity"
-    condition_monitoring_query_language {
-      duration = "600s"
-      query    = <<-EOT
-      fetch
-      generic_task :: custom.googleapis.com/opencensus/en-verification-server/api/issue/realm_token_latest
-      | {
-        AVAILABLE: filter metric.state == 'AVAILABLE' | align
-        ;
-        LIMIT: filter metric.state == 'LIMIT' | align
-      }
-      | group_by [metric.realm], [val: sum(value.realm_token_latest)]
-      | ratio
-      | window 1m
-      | condition ratio < 0.1
-      EOT
-      trigger {
-        count = 1
-      }
-    }
-  }
-  documentation {
-    content   = <<-EOT
-    ## $${policy.display_name}
+# resource "google_monitoring_alert_policy" "RealmTokenRemainingCapacityLow" {
+#   project      = var.verification-server-project
+#   combiner     = "OR"
+#   display_name = "RealmTokenRemainingCapacityLow"
+#   conditions {
+#     display_name = "Per-realm issue API token remaining capacity"
+#     condition_monitoring_query_language {
+#       duration = "600s"
+#       query    = <<-EOT
+#       fetch
+#       generic_task :: custom.googleapis.com/opencensus/en-verification-server/api/issue/realm_token_latest
+#       | {
+#         AVAILABLE: filter metric.state == 'AVAILABLE' | align
+#         ;
+#         LIMIT: filter metric.state == 'LIMIT' | align
+#       }
+#       | group_by [metric.realm], [val: sum(value.realm_token_latest)]
+#       | ratio
+#       | window 1m
+#       | condition ratio < 0.1
+#       EOT
+#       trigger {
+#         count = 1
+#       }
+#     }
+#   }
+#   documentation {
+#     content   = <<-EOT
+#     ## $${policy.display_name}
 
-    Realm $${metric.label.realm} daily verification code issuing remaining capacity below 10%.
-    EOT
-    mime_type = "text/markdown"
-  }
-  notification_channels = [
-    google_monitoring_notification_channel.email.id,
-  ]
+#     Realm $${metric.label.realm} daily verification code issuing remaining capacity below 10%.
+#     EOT
+#     mime_type = "text/markdown"
+#   }
+#   notification_channels = [
+#     google_monitoring_notification_channel.email.id,
+#   ]
 
-  depends_on = [
-    null_resource.manual-step-to-enable-workspace,
-  ]
-}
+#   depends_on = [
+#     null_resource.manual-step-to-enable-workspace,
+#   ]
+# }
 
 resource "google_monitoring_alert_policy" "backend_latency" {
   count        = var.https-forwarding-rule == "" ? 0 : 1
