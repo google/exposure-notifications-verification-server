7b731f8e2963ed31389968a20e7ab27c1aa4833b
Yuri Grinshteyn <ygrinshteyn@google.com>
2020-11-13 10:02:17 -0800

    adding SLO

--- terraform/alerting/verification-server/slos.tf
+++ terraform/alerting/verification-server/slos.tf
@@ -15,4 +15,33 @@
 resource "google_monitoring_custom_service" "verification-server" {
   service_id = "verification-server"
   display_name = "Verification Server"
+  project = var.monitoring-host-project
+}
+
+resource "google_monitoring_slo" "availability-slo" {
+  # the basics
+  service = google_monitoring_custom_service.verification-server.service_id
+  slo_id = "availability-slo"
+  display_name = "99.9% of requests are successful over rolling 28 days"
+
+  # the SLI
+  request_based_sli {
+    good_total_ratio {
+      good_service_filter = join(" AND ", [
+        "metric.type=\"loadbalancing.googleapis.com/https/request_count\"",
+        "resource.type=\"https_lb_rule\"",
+        "resource.label.\"backend_name\"=\"apiserver\"",
+        "metric.label.\"response_code_class\"=\"200\""
+      ])
+      bad_service_filter = join(" AND ", [
+        "metric.type=\"loadbalancing.googleapis.com/https/request_count\"",
+        "resource.type=\"https_lb_rule\"",
+        "resource.label.\"backend_name\"=\"apiserver\"",
+        "metric.label.\"response_code_class\"=\"500\""
+    }
+  }
+
+  # the goal
+  goal = 0.999
+  rolling_period_days = 28
 }
