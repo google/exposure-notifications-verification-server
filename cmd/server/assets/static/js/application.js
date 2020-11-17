a55ea68b4be4d4e704aed7fdc7b5f5e07b95d3e5
Weston Haught <whaught@google.com>
2020-11-17 09:44:31 -0800

    Switch var to let for function-scoped javascript

--- cmd/server/assets/static/js/application.js
+++ cmd/server/assets/static/js/application.js
@@ -255,17 +255,17 @@ $(function() {
 
 
 function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
+  let d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  var expires = "expires=" + d.toUTCString();
+  let expires = "expires=" + d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires;
 }
 
 function getCookie(cname) {
-  var name = cname + "=";
-  var ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
     while (c.charAt(0) == ' ') {
       c = c.substring(1);
     }
