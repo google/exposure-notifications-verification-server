ba8111e1a10371c68e9721894f354844b800722d
Weston Haught <whaught@google.com>
2020-11-19 11:04:17 -0800

    const

--- cmd/server/assets/static/js/application.js
+++ cmd/server/assets/static/js/application.js
@@ -276,7 +276,7 @@ function getCookie(cname) {
 }
 
 // Common error codes which should cancel the whole upload.
-let stopUploadingCodes = [
+const stopUploadingCodes = [
   '403', // forbidden
   '404', // not-found
   '503', // unavailable
