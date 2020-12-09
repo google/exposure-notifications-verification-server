b6c6f3fda280bdddc15adb40e07bc100a56f31d2
Yuchen Ying <ych@google.com>
2020-12-09 11:27:21 -0800

    Use the new security policy on Cloud Run services with load balancer mapping.

--- terraform/service_apiserver.tf
+++ terraform/service_apiserver.tf
@@ -197,6 +197,7 @@ resource "google_compute_backend_service" "apiserver" {
   backend {
     group = google_compute_region_network_endpoint_group.apiserver[0].id
   }
+  security_policy = google_compute_security_policy.cloud-armor.name
 }
 
 resource "google_cloud_run_service_iam_member" "apiserver-public" {
