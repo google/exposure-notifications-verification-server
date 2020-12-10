b73ed54cbab81e7ad19ff36367fa5d7050754dee
Weston Haught <whaught@google.com>
2020-12-09 16:29:03 -0800

    Include empty Apps

--- pkg/controller/associated/ios.go
+++ pkg/controller/associated/ios.go
@@ -33,6 +33,7 @@ type IOSData struct {
 }
 
 type Applinks struct {
+	Apps    []string `json:"details"`
 	Details []Detail `json:"details,omitempty"`
 }
 
@@ -84,6 +85,7 @@ func (c *Controller) getIosData(region string) (*IOSData, error) {
 
 	return &IOSData{
 		Applinks: Applinks{
+			Apps:    []string{}, // expected always empty.
 			Details: details,
 		},
 	}, nil
