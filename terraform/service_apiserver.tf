05ae1754267ab382e7c2775a2c66cfaccddd32f8
Yuchen Ying <ych@google.com>
2020-12-16 13:56:51 -0800

    Do not ignore annotation changes.
    The annotation will be used to do the ingress control (currently EAP).
    
    Part of #895

--- terraform/service_apiserver.tf
+++ terraform/service_apiserver.tf
@@ -142,7 +142,9 @@ resource "google_cloud_run_service" "apiserver" {
 
     metadata {
       annotations = {
+        "autoscaling.knative.dev/maxScale" : 1000
         "run.googleapis.com/vpc-access-connector" : google_vpc_access_connector.connector.id
+        "run.googleapis.com/vpc-access-egress" : "private-ranges-only"
       }
     }
   }
@@ -166,7 +168,9 @@ resource "google_cloud_run_service" "apiserver" {
 
   lifecycle {
     ignore_changes = [
-      template[0].metadata[0].annotations,
+      template[0].metadata[0].annotations["client.knative.dev/user-image"],
+      template[0].metadata[0].annotations["run.googleapis.com/client-name"],
+      template[0].metadata[0].annotations["run.googleapis.com/client-version"],
       template[0].spec[0].containers[0].image,
     ]
   }
--- terraform/service_modeler.tf
+++ terraform/service_modeler.tf
@@ -133,7 +133,9 @@ resource "google_cloud_run_service" "modeler" {
 
     metadata {
       annotations = {
+        "autoscaling.knative.dev/maxScale" : 1000
         "run.googleapis.com/vpc-access-connector" : google_vpc_access_connector.connector.id
+        "run.googleapis.com/vpc-access-egress" : "private-ranges-only"
       }
     }
   }
@@ -157,7 +159,9 @@ resource "google_cloud_run_service" "modeler" {
 
   lifecycle {
     ignore_changes = [
-      template[0].metadata[0].annotations,
+      template[0].metadata[0].annotations["client.knative.dev/user-image"],
+      template[0].metadata[0].annotations["run.googleapis.com/client-name"],
+      template[0].metadata[0].annotations["run.googleapis.com/client-version"],
       template[0].spec[0].containers[0].image,
     ]
   }
