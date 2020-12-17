60d7d4460bbfa8278c0264666107bcb947f89a43
Chao Dai <45011425+chaodaiG@users.noreply.github.com>
2020-12-17 15:55:37 -0800

    Terraform destroy first remove database from state (#1427)

--- scripts/ci-terraform.sh
+++ scripts/ci-terraform.sh
@@ -141,6 +141,8 @@ function destroy() {
     gcloud sql instances delete ${db_inst_name} -q --project=${PROJECT_ID} || best_effort
   fi
   # Clean up states after manual DB delete
+  terraform state rm module.en.google_sql_database_instance.db-inst || best_effort
+  terraform state rm module.en.google_sql_database.db || best_effort
   terraform state rm module.en.google_sql_user.user || best_effort
   terraform state rm module.en.google_sql_ssl_cert.db-cert || best_effort
 
