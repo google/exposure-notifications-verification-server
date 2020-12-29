6c0df3ce922b9d19d7b2b24d72a22a2c287c2ba0
Seth Vargo <seth@sethvargo.com>
2020-12-29 12:07:55 -0500

    Add tests for current_path middleware

---
+++ pkg/controller/middleware/current_path_test.go
@@ -0,0 +1,50 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package middleware_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/google/exposure-notifications-verification-server/pkg/controller"
+	"github.com/google/exposure-notifications-verification-server/pkg/controller/middleware"
+)
+
+func TestInjectCurrentPath(t *testing.T) {
+	t.Parallel()
+
+	injectCurrentPath := middleware.InjectCurrentPath()
+
+	r := httptest.NewRequest("GET", "/", nil)
+	r.Header.Set("Accept", "application/json")
+
+	w := httptest.NewRecorder()
+
+	// Verify the proper fields are added to the template map.
+	injectCurrentPath(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		m := controller.TemplateMapFromContext(ctx)
+
+		pth, ok := m["currentPath"]
+		if !ok {
+			t.Fatalf("expected currentPath to be populated in template map")
+		}
+
+		if _, ok := pth.(*middleware.Path); !ok {
+			t.Fatalf("expected %T to be *middleware.Path", pth)
+		}
+	})).ServeHTTP(w, r)
+}
