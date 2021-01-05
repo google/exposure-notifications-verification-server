24160aa22607e7ed3ef714d442dfbbbb09e3c0db
Yuri Grinshteyn <ygrinshteyn@google.com>
2021-01-05 11:33:48 -0800

    formatted slos.tf

--- terraform/alerting/slos.tf
+++ terraform/alerting/slos.tf
@@ -14,36 +14,36 @@
 
 locals {
   default_per_service_slo = {
-    enable_alert            = false
-    availability_goal       = 0.995
-    enable_latency_slo      = false
-    latency_goal            = 0.95
-    latency_threshold       = 60000 # 60 seconds, in ms
-    enable_latency_alert    = false
-    latency_alert_duration  = 300000 # 5 minutes, in ms
-    
+    enable_alert           = false
+    availability_goal      = 0.995
+    enable_latency_slo     = false
+    latency_goal           = 0.95
+    latency_threshold      = 60000 # 60 seconds, in ms
+    enable_latency_alert   = false
+    latency_alert_duration = 300000 # 5 minutes, in ms
+
   }
   service_configs = {
     adminapi = merge(local.default_per_service_slo,
-      { enable_alert = true,
-    enable_latency_alert = true,
-    enable_latency_slo = false,
+      { enable_alert         = true,
+        enable_latency_alert = true,
+        enable_latency_slo   = false,
     latency_threshold = 6000 })
     apiserver = merge(local.default_per_service_slo,
-      { enable_alert = true,
-    enable_latency_alert = true,
+      { enable_alert         = true,
+        enable_latency_alert = true,
     latency_threshold = 2000 })
     appsync    = local.default_per_service_slo
     cleanup    = local.default_per_service_slo
     e2e-runner = local.default_per_service_slo
     enx-redirect = merge(local.default_per_service_slo,
-      { enable_alert = true,
-    enable_latency_alert = true,
+      { enable_alert         = true,
+        enable_latency_alert = true,
     latency_threshold = 2000 })
     modeler = local.default_per_service_slo
     server = merge(local.default_per_service_slo,
-      { enable_alert = true,
-    enable_latency_alert = true,
+      { enable_alert         = true,
+        enable_latency_alert = true,
     latency_threshold = 2000 })
   }
 }
@@ -65,7 +65,7 @@ module "latency-alerts" {
 
   notification_channels = google_monitoring_notification_channel.channels
 
-  project = var.project
+  project           = var.project
   for_each          = merge(local.service_configs, var.slo_thresholds_overrides)
   custom_service_id = each.key
   enabled           = each.value.enable_latency_alert
