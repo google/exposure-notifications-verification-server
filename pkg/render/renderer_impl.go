5fc6d7295bc26aded98da97c6cfdd670025a27ef
Weston Haught <whaught@google.com>
2021-01-11 14:56:38 -0800

    no panicking

--- pkg/render/renderer_impl.go
+++ pkg/render/renderer_impl.go
@@ -334,11 +334,17 @@ func templateFuncs() htmltemplate.FuncMap {
 
 func hasOne(a interface{}) bool {
 	s := reflect.ValueOf(a)
+	if s.Kind() != reflect.Slice && s.Kind() != reflect.Array {
+		return false
+	}
 	return s.Len() == 1
 }
 
 func hasMany(a interface{}) bool {
 	s := reflect.ValueOf(a)
+	if s.Kind() != reflect.Slice && s.Kind() != reflect.Array {
+		return false
+	}
 	return s.Len() > 1
 }
 
