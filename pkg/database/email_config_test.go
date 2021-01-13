16c6a677a832eddc9f408c72d1906f102ae0bdd8
Weston Haught <whaught@google.com>
2021-01-13 15:12:58 -0800

    Test email/sms validation and system (#1587)
    * test email/sms validation and system
    
    * don't parallel these - they interfere
    
    * parallel the other for
    
    * keep that

--- pkg/database/email_config_test.go
+++ pkg/database/email_config_test.go
@@ -15,10 +15,54 @@
 package database
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/google/exposure-notifications-verification-server/pkg/email"
 )
+
+func TestEmailConfig_Validate(t *testing.T) {
+	t.Parallel()
+
+	db, _ := testDatabaseInstance.NewDatabase(t, nil)
+
+	realm, err := db.FindRealm(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cases := []struct {
+		name        string
+		emailConfig *EmailConfig
+		err         string
+	}{
+		{
+			name: "missing password and host",
+			emailConfig: &EmailConfig{
+				RealmID:      realm.ID,
+				ProviderType: email.ProviderType(email.ProviderTypeSMTP),
+				SMTPAccount:  "noreply@sendemails.meh",
+			},
+			err: "validation failed",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			err := db.SaveEmailConfig(tc.emailConfig)
+
+			if tc.err == "" {
+				if err != nil {
+					t.Error(err)
+				}
+			} else if err == nil || !strings.Contains(err.Error(), tc.err) {
+				t.Errorf("expected error. got %w, want %s", err, tc.err)
+			}
+		})
+	}
+}
 
 func TestEmailConfig_Lifecycle(t *testing.T) {
 	t.Parallel()
@@ -147,5 +191,58 @@ func TestEmailProvider(t *testing.T) {
 	}
 	if provider == nil {
 		t.Errorf("expected %v to be not nil", provider)
+	}
+}
+
+func TestSystemEmailProvider(t *testing.T) {
+	db, _ := testDatabaseInstance.NewDatabase(t, nil)
+
+	realm := NewRealmWithDefaults("test-email-realm-1")
+	if err := db.SaveRealm(realm, SystemTest); err != nil {
+		t.Fatal(err)
+	}
+
+	provider, err := db.SystemEmailConfig()
+	if !IsNotFound(err) {
+		t.Fatal(err)
+	}
+	if provider != nil {
+		t.Errorf("expected %v to be %v", provider, nil)
+	}
+
+	emailConfig := &EmailConfig{
+		RealmID:      realm.ID,
+		ProviderType: email.ProviderType(email.ProviderTypeSMTP),
+		SMTPAccount:  "noreply@sendemails.meh",
+		SMTPPassword: "my-secret-ref",
+		SMTPHost:     "smtp.sendemails.meh",
+		IsSystem:     true,
+	}
+	if err := db.SaveEmailConfig(emailConfig); err != nil {
+		t.Fatal(err)
+	}
+
+	provider, err = db.SystemEmailConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if provider == nil {
+		t.Errorf("expected %v to be not nil", provider)
+	}
+
+	// Empty creds to delete
+	emailConfig.SMTPHost = ""
+	emailConfig.SMTPAccount = ""
+	emailConfig.SMTPPassword = ""
+	if err := db.SaveEmailConfig(emailConfig); err != nil {
+		t.Fatal(err)
+	}
+
+	provider, err = db.SystemEmailConfig()
+	if !IsNotFound(err) {
+		t.Fatal(err)
+	}
+	if provider != nil {
+		t.Errorf("expected %v to be %v", provider, nil)
 	}
 }
