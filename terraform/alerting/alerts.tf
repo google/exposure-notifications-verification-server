e880af4ffb87e16eebd4ff6c39a27500035dd24c
Marilia Melo <mariliamelo@gmail.com>
2021-01-25 14:18:00 -0800

    Change CloudSchedulerJobFailed duration to 1s (#1682)
    * Change CloudSchedulerJobFailed duration to 1s
    
    * Update alerts.tf
    
    Co-authored-by: Seth Vargo <seth@sethvargo.com>

--- terraform/alerting/alerts.tf
+++ terraform/alerting/alerts.tf
@@ -190,9 +190,8 @@ resource "google_monitoring_alert_policy" "CloudSchedulerJobFailed" {
   conditions {
     display_name = "Cloud Scheduler Job Error Ratio"
     condition_monitoring_query_language {
-      duration = "900s"
-      # Uses rate(5m). See the reasoning above.
-      query = <<-EOT
+      duration = "1s"
+      query    = <<-EOT
       fetch cloud_scheduler_job::logging.googleapis.com/log_entry_count
       | filter (metric.severity == 'ERROR')
       | align rate(5m)
