ecf3f7997b49cdfa7a6af93b3b56477a89bb538e
Mike Helmick <helmick@google.com>
2021-02-01 18:13:05 -0800

    don't pass redis password when auth disabled

--- terraform/services.tf
+++ terraform/services.tf
@@ -34,7 +34,7 @@ locals {
     CACHE_HMAC_KEY       = "secret://${google_secret_manager_secret_version.cache-hmac-key.id}"
     CACHE_REDIS_HOST     = google_redis_instance.cache.host
     CACHE_REDIS_PORT     = google_redis_instance.cache.port
-    CACHE_REDIS_PASSWORD = "secret://${google_secret_manager_secret_version.redis-auth.id}"
+    CACHE_REDIS_PASSWORD = var.redis_enable_auth ? "secret://${google_secret_manager_secret_version.redis-auth.id}" : ""
   }
 
   database_config = {
