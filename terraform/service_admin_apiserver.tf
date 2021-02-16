df482ae11d11eb68a596b636641bacf4ff995f18
Mike Helmick <helmick@google.com>
2021-02-15 22:57:50 -0800

    admin API needs signing config to get key manager settings

--- terraform/service_admin_apiserver.tf
+++ terraform/service_admin_apiserver.tf
@@ -94,6 +94,7 @@ resource "google_cloud_run_service" "adminapi" {
             local.gcp_config,
             local.rate_limit_config,
             local.issue_config,
+            local.signing_config,
             local.observability_config,
 
             // This MUST come last to allow overrides!
