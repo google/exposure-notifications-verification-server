e709f4aa3dcf812d9deaba2cf99e0121747a699c
Mike Helmick <helmick@google.com>
2021-04-02 10:48:24 -0700

    capture OS in request context from user agent (#1973)

---
+++ pkg/controller/middleware/operating_system.go
@@ -0,0 +1,51 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/google/exposure-notifications-verification-server/pkg/controller"
+	"github.com/gorilla/mux"
+)
+
+func AddOperatingSystemFromUserAgent() mux.MiddlewareFunc {
+	userAgents := map[string]controller.OperatingSystem{
+		"darwin": controller.IOS,
+		"iphone": controller.IOS,
+		"dalvik": controller.Android,
+	}
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			agent := strings.ToLower(r.UserAgent())
+
+			osToSet := controller.UnknownOS
+			for k, os := range userAgents {
+				if strings.Contains(agent, k) {
+					osToSet = os
+					break
+				}
+			}
+
+			ctx = controller.WithOperatingSystem(ctx, osToSet)
+			r = r.Clone(ctx)
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
