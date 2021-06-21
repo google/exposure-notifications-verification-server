179ae80eb2ac3d064f0147f4c8982c28c23d9878
Seth Vargo <seth@sethvargo.com>
2021-06-21 15:14:58 -0400

    Don't use private javascript methods
    Our current transpiler doesn't support private methods yet

--- assets/server/static/js/_flash.js
+++ assets/server/static/js/_flash.js
@@ -10,63 +10,64 @@
     }
 
     clear() {
-      while (this.#container && this.#container.firstChild)
+      while (this.#container && this.#container.firstChild) {
         this.#container.removeChild(this.#container.firstChild);
+      }
     }
 
     error(message) {
-      this.#flash("error", message);
+      this.flash('error', message);
     }
 
     warning(message) {
-      this.#flash("warning", message);
+      this.flash('warning', message);
     }
 
     alert(message) {
-      this.#flash("alert", message);
+      this.flash('alert', message);
     }
 
-    #flash(level, message) {
+    flash(level, message) {
       const alert = this.#template.cloneNode(true);
 
-      const body = alert.querySelector(".alert-message");
+      const body = alert.querySelector('.alert-message');
       body.textContent = message;
 
-      const icon = alert.querySelector(".oi");
+      const icon = alert.querySelector('.oi');
 
       switch (level) {
-        case "error":
-          alert.classList.add("alert-danger");
-          icon.classList.add("oi-warning");
+        case 'error':
+          alert.classList.add('alert-danger');
+          icon.classList.add('oi-warning');
           break;
-        case "warning":
-          alert.classList.add("alert-warning");
-          icon.classList.add("oi-warning");
+        case 'warning':
+          alert.classList.add('alert-warning');
+          icon.classList.add('oi-warning');
           break;
-        case "alert":
-          alert.classList.add("alert-success");
-          icon.classList.add("oi-check");
+        case 'alert':
+          alert.classList.add('alert-success');
+          icon.classList.add('oi-check');
           break;
         default:
           throw `invalid flash level ${level}`;
       }
 
       this.#container.appendChild(alert).focus();
-      alert.classList.remove("d-none");
+      alert.classList.remove('d-none');
     }
   }
 
   // Add proper classes to all links inside alerts that were rendered with the
   // page (server-side).
-  window.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener('DOMContentLoaded', () => {
     // alertsContainer is the container div for all alerts. alertTemplate is the
     // cloneable HTML fragement from which other alerts are generated.
-    const alertsContainer = document.querySelector("body div#alerts-container");
+    const alertsContainer = document.querySelector('body div#alerts-container');
     if (!alertsContainer) {
       return;
     }
 
-    const alertTemplate = alertsContainer.querySelector("div#alert-template");
+    const alertTemplate = alertsContainer.querySelector('div#alert-template');
     if (!alertTemplate) {
       return;
     }
